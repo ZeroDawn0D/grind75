9312547a6a9239c626fa975412c29ddf2ac475db
Umang Majumder <54509868+ZeroDawn0D@users.noreply.github.com>
2024-06-12 12:18:02 +0530

    Create W1-12.cpp

---
+++ Cpp/W1-12.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        if(head==nullptr) return false;
+        while(true){
+            slow = slow->next;
+            if(slow==nullptr) return false;
+            fast = fast->next;
+            if(fast==nullptr) return false;
+            fast = fast->next;
+            if(fast==nullptr) return false;
+            if(slow == fast) return true;
+        }
+    }
+};
