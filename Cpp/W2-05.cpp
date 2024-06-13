9e9f4f595515365f63e51d5189a6f4354b2e00c1
Umang Majumder <54509868+ZeroDawn0D@users.noreply.github.com>
2024-06-13 17:23:03 +0530

    Update W2-05.cpp

--- Cpp/W2-05.cpp
+++ Cpp/W2-05.cpp
@@ -24,3 +24,37 @@ public:
         return new_head;
     }
 };
+
+ //iterative
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* new_head = head;
+        ListNode* prev_head = nullptr;
+        ListNode* next_head;
+        bool first = true;
+        if(head!=nullptr)
+            next_head = head->next;
+        while(true){
+            if(head==nullptr){
+                break;
+            }
+            else if(next_head==nullptr){
+                head->next = prev_head;
+                new_head = head;
+                break;
+            }
+            else{
+                prev_head = head;
+                head = next_head;
+                next_head = head->next;
+                head->next = prev_head;
+                if(first){
+                    first = false;
+                    prev_head->next = nullptr;
+                }
+            }
+        }
+        return new_head;
+    }
+};
