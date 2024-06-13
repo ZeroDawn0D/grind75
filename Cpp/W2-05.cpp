89556cf25b7ef0f72358e18d6f3cf9bed7b09d5f
Umang Majumder <54509868+ZeroDawn0D@users.noreply.github.com>
2024-06-13 16:57:43 +0530

    Create W2-05.cpp

---
+++ Cpp/W2-05.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr || head->next==nullptr){
+            return head;
+        }
+        ListNode* new_head = reverseList(head->next);
+        //ListNode* rest = head->next;
+        //rest->next = head;
+        head->next->next = head;
+        head->next = nullptr;
+        return new_head;
+    }
+};
